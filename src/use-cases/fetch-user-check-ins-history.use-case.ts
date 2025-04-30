b4464804b201bebedd7a4b5d7c2c7d71bfd8c1d6
leonan123 <leonanorth3@gmail.com>
2025-04-30 00:20:50 -0300

    new: fetch user check ins history use case

---
+++ src/use-cases/fetch-user-check-ins-history.use-case.ts
@@ -0,0 +1,30 @@
+import type { CheckIn } from 'generated/prisma'
+
+import type { CheckInsRepository } from '@/repositories/check-ins.repository'
+
+interface FetchUserCheckInsHistoryUseCaseRequest {
+  userId: string
+  page: number
+}
+
+interface FetchUserCheckInsHistoryUseCaseResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistoryUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async handle({
+    userId,
+    page,
+  }: FetchUserCheckInsHistoryUseCaseRequest): Promise<FetchUserCheckInsHistoryUseCaseResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
+
+    return {
+      checkIns,
+    }
+  }
+}
