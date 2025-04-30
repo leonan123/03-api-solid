84dcd48632ad6b9bd76a14b7ab2936727cabb97b
leonan123 <leonanorth3@gmail.com>
2025-04-30 00:32:46 -0300

    new: get user metrics use case

--- src/repositories/in-memory/in-memory-check-ins.repository.ts
+++ src/repositories/in-memory/in-memory-check-ins.repository.ts
@@ -8,6 +8,10 @@ import { CheckInsRepository } from './../check-ins.repository'
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
 
+  async countByUserId(userId: string) {
+    return this.items.filter((item) => item.user_id === userId).length
+  }
+
   async findManyByUserId(userId: string, page: number) {
     return this.items
       .filter((item) => item.user_id === userId)
---
+++ src/use-cases/get-user-metrics.use-case.spec.ts
@@ -0,0 +1,33 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins.repository'
+
+import { GetUserMetricsUseCase } from './get-user-metrics.use-case'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: GetUserMetricsUseCase
+
+describe('Get User Metrics Use Case', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new GetUserMetricsUseCase(checkInsRepository)
+  })
+
+  it('should be able to get check-ins count', async () => {
+    await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-01',
+    })
+
+    await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-02',
+    })
+
+    const { checkInsCount } = await sut.handle({
+      userId: 'user-01',
+    })
+
+    expect(checkInsCount).toEqual(2)
+  })
+})
